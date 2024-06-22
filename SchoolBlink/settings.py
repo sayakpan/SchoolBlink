28a61a986b62e69a00ed666befc3d2fea569ebb9
Sayak Pan <sayakpan9635@gmail.com>
2024-06-22 19:32:46 +0000

    settings modified

--- SchoolBlink/settings.py
+++ SchoolBlink/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-yn5mj*l!v2j)3!u*qpd0o)(gods+)qq=_&txr+)i#(x(g$i$fj
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Custom User Model
 
@@ -120,11 +120,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Python Anywhere Settings
